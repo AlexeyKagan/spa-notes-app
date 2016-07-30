7ace4d7576b3279932283f2625fd932581f7169f
AlexeyKagan <tiko1vg@gmail.com>
2016-07-30 23:56:32 +0300

    configured webpack for front-end part application

--- webpack.config.js
+++ webpack.config.js
@@ -11,44 +11,20 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
-                loader: "babel",
-                exclude: [/node_modules/, /public/]
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader!autoprefixer-loader",
-                exclude: [/node_modules/, /public/]
-            },
-            // {
-            //     test: /\.less$/,
-            //     loader: "style-loader!css-loader!autoprefixer-loader!less",
-            //     exclude: [/node_modules/, /public/]
-            // },
-            {
-                test: /\.gif$/,
-                loader: "url-loader?limit=10000&mimetype=image/gif"
-            },
-            {
-                test: /\.jpg$/,
-                loader: "url-loader?limit=10000&mimetype=image/jpg"
-            },
-            {
-                test: /\.png$/,
-                loader: "url-loader?limit=10000&mimetype=image/png"
-            },
-            {
-                test: /\.svg/,
-                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015']
+                }
             },
             {
                 test: /\.jsx$/,
-                loader: "react-hot!babel",
-                exclude: [/node_modules/, /public/]
-            },
-            {
-                test: /\.json$/,
-                loader: "json-loader"
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015']
+                }
             }
         ]
     }
-}
+};
